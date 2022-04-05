ddd2bc7d6c0eb67df42509d5605ac1652dc96b17
ngvuong <vminguyen@ucdavis.edu>
2022-04-04 23:58:41 -0700

    Update nav menu interaction

--- public/javascripts/navMenu.js
+++ public/javascripts/navMenu.js
@@ -2,7 +2,16 @@
   const userIcon = document.querySelector('.user-icon');
   const navMenu = document.querySelector('.nav__list--sub');
 
-  userIcon.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-  });
+  userIcon.addEventListener(
+    'click',
+    () => {
+      navMenu.classList.toggle('active');
+      document.addEventListener('click', (e) => {
+        if (e.target !== userIcon) {
+          navMenu.classList.remove('active');
+        }
+      });
+    },
+    { bubbles: false }
+  );
 })();
